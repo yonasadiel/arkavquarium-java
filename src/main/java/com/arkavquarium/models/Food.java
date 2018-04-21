7bec7857de1e4ede18a354b5af62858c5454316a
dafifaraz <30387504+dafifaraz@users.noreply.github.com>
2018-04-21 16:51:11 +0700

    updated position, comments follow javadoc std

---
+++ src/main/java/com/arkavquarium/models/Food.java
@@ -0,0 +1,48 @@
+public class Food implements Drawable {
+    private static final String assetPath = "assets/img/food.png";
+    private static final int PRICE = 10;
+    /**
+    * Max velocity of the food movement
+    */
+    private static int MAX_VELOCITY = 30;
+    private Position position;
+    /**
+     * Construct Food at destined position
+     * @param x absis of food
+     * @param x ordinate of food
+     */
+    public Food(double x, double y){
+        this.position = new Position(x, y);
+    }
+
+    /**
+     * Move Food to bottom
+     * @param y minimum height
+     * @param elapsedSeconds elapsed seconds
+     */
+    public void move(int y, double elapsedSeconds){
+        Position bottom = new Position(0, y);
+        this.position.moveVertical(bottom, elapsedSeconds * MAX_VELOCITY);
+    }
+
+    /**
+    * @return food position 
+    **/
+    Position getPosition(){
+        return this.position;
+    }
+
+    /**
+     * @return asset path
+     */
+    String getAssetPath(){
+        return assetPath;
+    }
+
+    /**
+     * @return food price
+     */
+    final int getPrice(){
+        return PRICE;
+    }
+}
